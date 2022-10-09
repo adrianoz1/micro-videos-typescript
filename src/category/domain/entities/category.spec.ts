afffa8001c8a4a2eae6ab54cb10fba06619fa67d
Adriano Almeida <adriano@z1.app>
2022-10-09 20:45:33 -0300

    test: entity category

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -2,7 +2,18 @@ import { Category } from "./category"
 
 describe("Category Test", () => {
   test("construct of category", () => {
-    const category = new Category('Movie')
-    expect(category.name).toBe('Movie')
+    //Arrange
+    const props ={
+      name: 'Movie',
+      description: 'some description',
+      is_active: true,
+      created_at: new Date
+    }
+
+    //Act
+    const category = new Category(props)
+
+    //Assert
+    expect(category.props).toStrictEqual(props)
   })
 })
