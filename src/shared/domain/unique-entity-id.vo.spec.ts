d6de4e8bfedea8c8d8b1202d7c2b309d86a6dffe
Adriano Almeida <adriano@z1.app>
2022-10-11 02:45:44 +0000

    chore: dockerfile

--- src/shared/domain/unique-entity-id.vo.spec.ts
+++ src/shared/domain/unique-entity-id.vo.spec.ts
@@ -2,15 +2,19 @@ import UniqueEntityId from "./unique-entity-id-vo";
 import InvalidUuidError from "../errors/invalid-uuid.error";
 import { v4 as uuidV4, validate as uuidValidate } from "uuid";
 
+const spyValidateMethod = () => {
+  return jest.spyOn(UniqueEntityId.prototype as any, "validate");
+};
+
 describe("UniqueEntityId Unit Tests", () => {
   it("should throw error when uuid is invalid", () => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
+    const validateSpy = spyValidateMethod();
     expect(() => new UniqueEntityId("fake id")).toThrow(new InvalidUuidError());
     expect(validateSpy).toHaveBeenCalled();
   });
 
   it("should accept a uuid passed in construct with id", () => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
+    const validateSpy = spyValidateMethod();
     const uuid = uuidV4();
     const vo = new UniqueEntityId(uuid);
     expect(vo.id).toBe(uuid);
@@ -18,7 +22,7 @@ describe("UniqueEntityId Unit Tests", () => {
   });
 
   it("should accept a uuid passed in construct without id is empty", () => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
+    const validateSpy = spyValidateMethod();
     const uuid = uuidV4();
     const vo = new UniqueEntityId();
     expect(uuidValidate(vo.id)).toBeTruthy();
